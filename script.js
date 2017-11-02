7f556390cfd6c07797c20ec2f9dc8a808dcb8828
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-02 22:00:11 +0000

    reset button added

--- script.js
+++ script.js
@@ -6,19 +6,30 @@ var xMoves = [];
 
 var winConditons = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
+$('#reset').click(function () {
+  $('.box').removeClass('X, O').html('').addClass('clear')
+  xMoves = []
+  oMoves = []
+  xTurn = true;
+  oTurn = false;
+  whatPlayer();
+
+})
+//x o grid inputs
 $('.box').click(function(){
   if (xTurn === true) {
-  //add X class to box
+  //add X class and x to box
   $(this).addClass('X').html('X')
   xMoves.push(parseInt($(this).attr('data-num')))
   console.log(xMoves, 'x');
   oTurn = true;
   xTurn = false;
   whatPlayer();
+  winnerCheck()
 
 
   } else if (oTurn === true) {
-    //add O class to box
+    //add O class and o to box
     $(this).addClass('O').html('O')
     oMoves.push(parseInt($(this).attr('data-num')))
     console.log(oMoves, 'o');
@@ -28,23 +39,19 @@ $('.box').click(function(){
   }
 });
 function winnerCheck() {
-  if (xMoves) {
-
+  for (var i = 0; i < winConditons.length; i++) {
+    if (winConditons[i] === xMoves){
+      alert('winner x')
+    } else if (winConditons[i] === oMoves) {
+      alert('winner y')
+    }
   }
 }
+winnerCheck()
 function whatPlayer() {
-  if (xTurn === true){
+  if (xTurn ===true){
     $('.playerTurn').html("It is X's turn")
   } else if (xTurn === false) {
     $('.playerTurn').html("It is O's turn")
   }
 }
-// $(winConditons).each(function (index, winningCombination) {
-//   console.log(winConditons, 'winConditons');
-//
-//   if (winningCombination[index] == xMoves) {
-//     console.log(winConditons);
-//
-//     alert('player won')
-//   }
-// })
