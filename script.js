18842fd30c10e8893dcf60d69102065a260f7279
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-11-02 22:23:58 +0000

    buttons can no longer be pressed twice

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $('#reset').click(function () {
 })
 //x o grid inputs
 $('.box').click(function(){
-  if (xTurn === true) {
+  if (xTurn === true && $(this).attr('class') === 'box') {
   //add X class and x to box
   $(this).addClass('X').html('X')
   xMoves.push(parseInt($(this).attr('data-num')))
@@ -28,7 +28,7 @@ $('.box').click(function(){
   winnerCheck()
 
 
-  } else if (oTurn === true) {
+  } else if (oTurn === true && $(this).attr('class') === 'box')  {
     //add O class and o to box
     $(this).addClass('O').html('O')
     oMoves.push(parseInt($(this).attr('data-num')))
@@ -40,6 +40,7 @@ $('.box').click(function(){
 });
 function winnerCheck() {
   for (var i = 0; i < winConditons.length; i++) {
+
     if (winConditons[i] === xMoves){
       alert('winner x')
     } else if (winConditons[i] === oMoves) {
